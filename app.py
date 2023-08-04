77d910184cdf6e1e303b671a9dd0183bdac14387
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-04 20:58:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,57 +2,43 @@ import streamlit as st
 import pandas as pd
 import xml.etree.ElementTree as ET
 from io import StringIO
+from faker import Faker
 
-# Custom anonymizeDF package (fictional placeholder)
-from anonymizeDF import anonymize_df
-
-def anonymize_xml_element(element, anonymized_df, idx):
-    # Here, you can implement the anonymization logic for each element.
-    # For simplicity, we'll just replace the element's text with "ANONYMIZED".
-    try:
-        element.text = anonymized_df.loc[idx, "text"]
-    except Exception as e:
-        print(e)
+def anonymize_xml_element(element, idx):
+    fake = Faker()
+    original_text = element.text
+    fake_text = fake.text(max_nb_chars=len(original_text))
+    element.text = fake_text
 
 def anonymize_xml(xml_content):
     tree = ET.ElementTree(ET.fromstring(xml_content))
     root = tree.getroot()
 
-    # Convert XML data to a DataFrame for anonymization
-    xml_as_df = []
-    for element in root.iter():
-        xml_as_df.append({"tag": element.tag, "text": element.text})
-    xml_df = pd.DataFrame(xml_as_df)
-
-    # Anonymize the DataFrame using the anonymizeDF package
-    anonymized_df = anonymize_df(xml_df, columns=["text"])
-
-    # Update the XML elements with anonymized data
     for idx, element in enumerate(root.iter()):
-        anonymize_xml_element(element, anonymized_df, idx)
+        try:
+            anonymize_xml_element(element, idx)
+        except Exception as e:
+            print(e)
 
-    # Convert the updated XML back to a string
     output = StringIO()
     tree.write(output, encoding="unicode")
     return output.getvalue()
 
 def main():
     st.title("XML Data Anonymizer")
-
     st.write("Upload an XML file below and click the 'Anonymize' button to remove sensitive data.")
-
-    # Upload XML file
+    
     uploaded_file = st.file_uploader("Upload XML File", type=["xml"])
 
     if uploaded_file is not None:
-        # Read the content of the uploaded XML file
         xml_content = uploaded_file.read()
 
         if st.button("Anonymize"):
-            # Anonymize the XML content
-            anonymized_xml = anonymize_xml(xml_content)
+            try:
+                anonymized_xml = anonymize_xml(xml_content)
+            except Exception as e:
+                st.error(e)
 
-            # Offer the anonymized XML as a download link
             st.download_button(
                 "Download Anonymized XML",
                 data=anonymized_xml,
