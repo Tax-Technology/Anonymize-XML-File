a21e349b8426730582202287c5d6e6f13a868cf8
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-04 15:52:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,64 @@
+import streamlit as st
+import pandas as pd  # Make sure to have pandas installed
+import xml.etree.ElementTree as ET
+from io import StringIO
 
+# Custom anonymizeDF package (fictional placeholder)
+from anonymizeDF import anonymize_df
+
+def anonymize_element(element):
+    # Here, you can implement the anonymization logic for each element.
+    # For simplicity, we'll just replace the element's text with "ANONYMIZED".
+    try:
+        element.text = anonymized_df.loc[idx, "text"]
+    except Exception as e:
+        print(e)
+
+def anonymize_xml(xml_content):
+    tree = ET.ElementTree(ET.fromstring(xml_content))
+    root = tree.getroot()
+
+    # Convert XML data to a DataFrame for anonymization
+    xml_as_df = []
+    for element in root.iter():
+        xml_as_df.append({"tag": element.tag, "text": element.text})
+    xml_df = pd.DataFrame(xml_as_df)
+
+    # Anonymize the DataFrame using the anonymizeDF package
+    anonymized_df = anonymize_df(xml_df, columns=["text"])
+
+    # Update the XML elements with anonymized data
+    for idx, element in enumerate(root.iter()):
+        anonymize_element(element)
+
+    # Convert the updated XML back to a string
+    output = StringIO()
+    tree.write(output, encoding="unicode")
+    return output.getvalue()
+
+def main():
+    st.title("XML Data Anonymizer")
+
+    st.write("Upload an XML file below and click the 'Anonymize' button to remove sensitive data.")
+
+    # Upload XML file
+    uploaded_file = st.file_uploader("Upload XML File", type=["xml"])
+
+    if uploaded_file is not None:
+        # Read the content of the uploaded XML file
+        xml_content = uploaded_file.read()
+
+        if st.button("Anonymize"):
+            # Anonymize the XML content
+            anonymized_xml = anonymize_xml(xml_content)
+
+            # Offer the anonymized XML as a download link
+            st.download_button(
+                "Download Anonymized XML",
+                data=anonymized_xml,
+                file_name="anonymized.xml",
+                mime="text/xml",
+            )
+
+if __name__ == "__main__":
+    main()
