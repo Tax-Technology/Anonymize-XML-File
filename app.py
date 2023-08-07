0eccfcbfb99dd6f86f5884ab4aa859f326f57f44
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-07 14:34:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import faker
 import xmltodict
+import io
 
 def anonymize_xml(xml_file):
     if xml_file is None:
@@ -21,12 +22,6 @@ def anonymize_xml(xml_file):
                     value[index] = faker_instance.text()
 
         anonymized_xml = xmltodict.unparse(data)
-
-        # Check if the anonymized data is a binary file
-        if not isinstance(anonymized_xml, bytes):
-            # Convert the anonymized data to a binary file
-            anonymized_xml = anonymized_xml.encode('utf-8')
-
         return anonymized_xml
     except Exception as e:
         st.error(e)
@@ -41,11 +36,20 @@ def main():
             anonymized_data = anonymize_xml(xml_file)
             st.progress(50)
 
-            st.download_button("Download Anonymized XML", data=anonymized_data, file_name="anonymized.xml")
+            # Encode the string as bytes
+            anonymized_data_bytes = anonymized_data.encode("utf-8")
+
+            # Create a link to download the file
+            st.markdown(get_download_link(anonymized_data_bytes, "anonymized.xml"), unsafe_allow_html=True)
 
             st.progress(100)
     else:
         st.error("Please upload an XML file.")
+
+def get_download_link(data, file_name):
+    b64 = base64.b64encode(data).decode()
+    href = f'<a href="data:file/txt;base64,{b64}" download="{file_name}">Click here to download {file_name}</a>'
+    return href
 
 if __name__ == "__main__":
     main()
